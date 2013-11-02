5ef1a0fe40bc9472d1f8b6577d368700852f701c
Sunil Shah <git@ss21k.co.uk>
2013-11-01 23:26:47 -0700

    Hello, world!

---
+++ src/main/java/uk/co/geekonabicycle/icalvisualiser/VisualiserServlet.java
@@ -0,0 +1,18 @@
+package uk.co.geekonabicycle.icalvisualiser;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class VisualiserServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Hello, world");
+    }
+	
+}
